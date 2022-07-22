ea0d99aebdd9b1276c07e0e428de1697eaba82d6
HoonJae <rlagnswo0505@naver.com>
2022-07-22 12:10:25 +0900

    laravel start

---
+++ app/Http/Controllers/BoardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BoardController extends Controller
+{
+    // p.77 라우트명의 관례
+
+    public function index() {
+      return view('board/index');
+    }
+    public function create() {
+      return view('board/create');
+    }
+}
